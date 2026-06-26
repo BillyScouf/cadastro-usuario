6b9808eb477eb3e00e7b9cbda301dbb4eca1b9f7
Andersio Lopes <billyscouf@gmail.com>
2026-06-26 10:23:01 +0100

    add model usuario

--- cadastro-usuario/src/main/java/com/andersio/cadastro_usuario/domain/Usuario.java
+++ cadastro-usuario/src/main/java/com/andersio/cadastro_usuario/domain/Usuario.java
@@ -1,34 +1,34 @@
-//package com.andersio.cadastro_usuario.domain;
-//
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import org.hibernate.annotations.CreationTimestamp;
-//
-//import java.time.LocalDateTime;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Builder
-//@Table(name = "user_tb")
-//public class Usuario {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @Column(name = "nome", nullable = false)
-//    private String nome;
-//
-//    @Column(name = "email", nullable = false, unique = true)
-//    private String email;
-//
-//    @Column(name = "senha", nullable = false)
-//    private String senha;
-//
-//    @CreationTimestamp
-//    private LocalDateTime createAt;
-//}
+package com.andersio.cadastro_usuario.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "user_tb")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
+
+    @CreationTimestamp
+    private LocalDateTime createAt;
+}
