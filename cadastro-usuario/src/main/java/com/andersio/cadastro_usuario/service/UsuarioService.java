bf1f341c13d031be73a8605dd813b7ce4aef6c66
Andersio Lopes <billyscouf@gmail.com>
2026-06-26 10:29:21 +0100

    add layer of service

---
+++ cadastro-usuario/src/main/java/com/andersio/cadastro_usuario/service/UsuarioService.java
@@ -0,0 +1,18 @@
+package com.andersio.cadastro_usuario.service;
+
+import com.andersio.cadastro_usuario.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+
+}
