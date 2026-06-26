67c308712d358ebad48b01716739f36f463e34cb
Andersio Lopes <billyscouf@gmail.com>
2026-06-26 22:09:50 +0100

    Adicionando o metodo para buscar usuario por id e por email no service

--- cadastro-usuario/src/main/java/com/andersio/cadastro_usuario/service/UsuarioService.java
+++ cadastro-usuario/src/main/java/com/andersio/cadastro_usuario/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.andersio.cadastro_usuario.service;
 
+import com.andersio.cadastro_usuario.domain.model.Usuario;
 import com.andersio.cadastro_usuario.dto.UsuarioResponseDTO;
 import com.andersio.cadastro_usuario.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -39,4 +41,23 @@ public class UsuarioService {
         }
 
     }
+
+    // get user to id
+    public UsuarioResponseDTO getUserToId(Long id){
+        Usuario usuario = usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuario Nao encontrado"));
+
+        return new UsuarioResponseDTO(usuario.getEmail());
+    }
+
+    // get user to email
+    public UsuarioResponseDTO getUserToEmail(String email){
+        Usuario user = usuarioRepository.findByEmail(email).orElseThrow(
+                () -> new RuntimeException("Usuario nao encontrado ou email invalido")
+        );
+
+        return new UsuarioResponseDTO(user.getEmail());
+    }
+
+    //
 }
